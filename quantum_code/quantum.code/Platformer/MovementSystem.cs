454ece5068bc1d8668bae5ba89ca0c1f5107859d
Yvan Cezarino Fernandes <yvan.fernandes@wildlifestudios.com>
2023-07-28 23:03:11 +0200

    Add tutorial stuff with character controller

---
+++ quantum_code/quantum.code/Platformer/MovementSystem.cs
@@ -0,0 +1,27 @@
+namespace Quantum.Platformer
+{
+    public unsafe class MovementSystem : SystemMainThreadFilter<MovementSystem.Filter>
+    {
+        public struct Filter
+        {
+            public EntityRef Entity;
+            public CharacterController3D* CharacterController;
+        }
+        
+        public override void Update(Frame f, ref Filter filter)
+        {
+            Input input = default;
+            if(f.Unsafe.TryGetPointer(filter.Entity, out PlayerLink* playerLink))
+            {
+                input = *f.GetPlayerInput(playerLink->Player);
+            }
+
+            if (input.Jump.WasPressed)
+            {
+                filter.CharacterController->Jump(f);
+            }
+
+            filter.CharacterController->Move(f, filter.Entity, input.Direction.XOY);
+        }
+    }
+}
